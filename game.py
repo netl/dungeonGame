c134877c213ad38651691425fd984c0689d16607
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2016-05-19 00:26:43 +0300

    added mobs, cleaned up a bunch and added debug messages

--- game.py
+++ game.py
@@ -4,44 +4,54 @@ from pygame.locals import *
 import random
 import time
 
+debug = 1
+if (debug): print("debug mode on!")
+
 #setup window and graphics
 class view:
    width = 800
    height = 600
+
 pygame.init()
 screen = pygame.display.set_mode([view.width,view.height])
 
 class world:
    grid = 40 #how many blocks per world vertically/horizontally
    viewGrid = 5 #how many blocks can the player see
-   players = 4 #how many players, duh.
+   players = 1 #how many players, duh.
 
    #different settings for different amounts of players
-   if(players==1): #player view on the left, map overview at the top right
+   #1 player
+   #player view on the left, map overview at the top right
+   if(players==1): 
       playerWindowSize = view.height
       playerWindowPosition = ((0,0),(0,0))
       squareSize = playerWindowSize/viewGrid
       mapScale = (view.width-playerWindowSize)/(squareSize*grid)
       miniMapPosition = (playerWindowSize,0)
 
-   elif(players==2): #player 1 on the top right, player 2 on the top left, map overview in bottom center
+   #2 players
+   #player 1 on the top right, player 2 on the top left, map overview in bottom center
+   elif(players==2): 
       playerWindowSize = view.width/2
       playerWindowPosition = ((view.width-playerWindowSize,0),(0,0))
       squareSize = playerWindowSize/viewGrid
       mapScale = (view.height-playerWindowSize)/(squareSize*grid)
       miniMapPosition = (playerWindowSize-(view.height-playerWindowSize)/2,playerWindowSize)
 
-   else: #player 1: top right, player 2: bottom left, player 3: top left, player 4: bottom right, map overview in center
+   #3-4 players
+   #player 1: top right, player 2: bottom left, player 3: top left, player 4: bottom right, map overview in center
+   else:
       playerWindowSize = view.height/2
       playerWindowPosition = ((view.width-playerWindowSize,0),(0,view.height-playerWindowSize),(0,0),(view.width-playerWindowSize,view.height-playerWindowSize))
       squareSize = playerWindowSize/viewGrid
       mapScale = (view.width-playerWindowSize*2)/(squareSize*grid)
       miniMapPosition = (playerWindowSize,(view.height-mapScale*squareSize*grid)/2)
 
-   miniMap = pygame.Surface((grid*squareSize*mapScale,grid*squareSize*mapScale)) #minimap
+   #map with the overview of the current level
+   miniMap = pygame.Surface((grid*squareSize*mapScale,grid*squareSize*mapScale))
 
 class level:
-   color = ((255,128,0),(0,0,0))
    def __init__(self, width, height, tileset):
       self.tileset = pygame.image.load("./"+tileset+".png")
       self.tileset = pygame.transform.scale(self.tileset,(int(2*world.squareSize),int(world.squareSize)))
@@ -54,80 +64,127 @@ class level:
          for x in range(len(self.map[y])):
             self.map[y][x] = 0
             world.miniMap.fill((0,0,0))
+      if (debug): print("level cleared")
       return(0)
    
+   #make sure nothing is at position
+   def isClear(self, x, y):
+      #check for players
+      for p in range(len(plr)):
+         if (plr[p].x == x and plr[p].y == y):
+            return(0)
+
+      #check for walls
+      if(self.map[y][x] == 0):
+         return(0)
+
+      #check for mobs
+      for m in range(len(self.mob)):
+         if (self.mob[m].x == x and self.mob[m].y == y):
+            return(0)
+
+      #all good!
+      return(1)
+
    #generate tunnels
    def generate(self):
-      miner = player(int(world.grid/2),int(world.grid/2))
+      x = int(world.grid/2)
+      y = int(world.grid/2)
+      
+      #move players to center
       for p in range(len(plr)):
-         plr[p].x = miner.x
-         plr[p].y = miner.y
+         plr[p].x = x
+         plr[p].y = y
+      if (debug): print("players moved to origin!")
+      
+      #start diggin'
       for i in range(world.grid*5):
-         self.map[miner.y][miner.x] = 1 
+         self.map[y][x] = 1 
          r = random.randint(0,3)
          stepCount = random.randint(0,5)
          if(r == 0):
-            while(miner.move('up') and miner.underneath() == 1 and stepCount<5):
-               pass
+            while(y > 0 and l.map[y][x] == 1 and stepCount < 5):
+               y = y-1
          elif(r == 1):
-            while(miner.move('down') and miner.underneath() == 1 and stepCount<5):
-               pass
+            while(y < (world.grid-1) and l.map[y][x] == 1 and stepCount < 5):
+               y = y+1
          elif(r == 2):
-            while(miner.move('left') and miner.underneath() == 1 and stepCount<5):
-               pass
+            while(x > 0 and l.map[y][x] == 1  and stepCount<5):
+               x = x-1
          elif(r == 3):
-            while(miner.move('right') and miner.underneath() == 1 and stepCount<5):
-               pass
+            while(x < (world.grid-1) and l.map[y][x] == 1 and stepCount < 5):
+               x = x+1
+      if (debug): print("digging finished at: %i,%i" % (x, y))
+
+      #create bad guys
+      self.mob = [player() for m in range(5)]
+      for m in range(len(self.mob)):
+         while( self.isClear(x, y) == 0):
+            x = random.randint(0, world.grid-1)
+            y = random.randint(0, world.grid-1)
+         self.mob[m].x = x
+         self.mob[m].y = y
+         if (debug): print("mob spawned at: %i,%i" % (self.mob[m].x, self.mob[m].y))
       return(0)
 
 class player:
-   def __init__(self,x,y):
+   def __init__(self):
       self.controls = {'up':K_UP,'down':K_DOWN,'left':K_LEFT,'right':K_RIGHT}
       self.mapColor = (255,255,255)
       self.perspective = pygame.Surface((world.playerWindowSize,world.playerWindowSize))
       self.view = [[0 for x in range(world.viewGrid)] for y in range(world.viewGrid)]
-      self.x = x
-      self.y = y
+      self.x = 0
+      self.y = 0
 
-   def readInput(self, keys): #do movement according to given key input
-      if key[self.controls['up']]:
-         self.move('up')
-         if(self.underneath() == 0):
-            self.move('down')
-         return(0)
-      if key[self.controls['down']]:
-         self.move('down')
-         if(self.underneath() == 0):
-            self.move('up')
-         return(0)
-      if key[self.controls['left']]:
-         self.move('left')
-         if(self.underneath() == 0):
-            self.move('right')
-         return(0)
-      if key[self.controls['right']]:
-         self.move('right')
-         if(self.underneath() == 0):
-            self.move('left')
-         return(0)
-      return(1)
-      
+   #do movement according to given key input
+   def readInput(self, keys):
+      if keys[self.controls['up']]:
+         if (debug): print("player wants to move up")
+         return(self.move('up'))
+      if keys[self.controls['down']]:
+         if (debug): print("player wants to move down")
+         return(self.move('down'))
+      if keys[self.controls['left']]:
+         if (debug): print("player wants to move left")
+         return(self.move('left'))
+      if keys[self.controls['right']]:
+         if (debug): print("player wants to move right")
+         return(self.move('right'))
+      return(2)
+    
+   #move, if possible
    def move(self, direction):
-      if(direction == 'up' and self.y>0):
+      if(direction == 'up' and self.y>0 and l.isClear(self.x, self.y-1) == 1):
          self.y = self.y - 1
-         return(1)
-      elif(direction == 'down' and self.y<len(l.map)-1):
-         self.y = self.y + 1
-         return(1)
-      if(direction == 'left' and self.x>0):
-         self.x = self.x - 1
-         return(1)
-      elif(direction == 'right' and self.x<len(l.map[self.y])-1):
-         self.x = self.x + 1
-         return(1)
-      else:
          return(0)
+      elif(direction == 'down' and self.y<len(l.map)-1 and l.isClear(self.x, self.y+1)):
+         self.y = self.y + 1
+         return(0)
+      elif(direction == 'left' and self.x>0 and l.isClear(self.x-1, self.y) == 1):
+         self.x = self.x - 1
+         return(0)
+      elif(direction == 'right' and self.x<len(l.map[self.y])-1 and l.isClear(self.x+1, self.y)):
+         self.x = self.x + 1
+         return(0)
+      else:
+         return(1)
 
+   #try to move towards given location
+   def moveTowards(self, x, y):
+      dx = x-self.x 
+      dy = y-self.y 
+      if (abs(dx) > abs(dy)):
+         if (dx > 0):
+            self.move('right')
+         else:
+            self.move('left')
+      else:
+         if (dy > 0):
+            self.move('down')
+         elif (dy < 0):
+            self.move('up')
+
+   #refresh viewpoint
    def updateView(self):
       for y in range(world.viewGrid):
          for x in range(world.viewGrid):
@@ -136,13 +193,15 @@ class player:
                self.view[y][x] = l.map[self.y+y+offset][self.x+x+offset]
             else:
                self.view[y][x] = 0
-      
-
+   
+   #chek what are you standing on (prolly not in use anymore)
    def underneath(self):
       return(l.map[self.y][self.x])
 
+   #render viewpoint
    def drawView(self):
       offset=self.perspective.get_width()/2-world.squareSize/2
+
       #draw the grid
       for y in range(world.viewGrid):
          for x in range(world.viewGrid):
@@ -155,11 +214,18 @@ class player:
       for p in range(len(plr)):
          pygame.draw.rect(self.perspective, plr[p].mapColor, pygame.Rect(world.squareSize*(-self.x+plr[p].x)+offset, world.squareSize*(-self.y+plr[p].y)+offset, world.squareSize,world.squareSize))
 
+      #draw bad mobs
+      for m in range(len(l.mob)):
+         pygame.draw.rect(self.perspective, l.mob[m].mapColor, pygame.Rect(world.squareSize*(-self.x+l.mob[m].x)+offset, world.squareSize*(-self.y+l.mob[m].y)+offset, world.squareSize,world.squareSize))
+
 #create level
 l = level(world.grid,world.grid,"tileset")
 
 #generate players
-plr = [player(int(world.grid/2),int(world.grid/2)) for p in range(world.players)]
+plr = [player() for p in range(world.players)]
+for p in range(len(plr)):
+   plr[p].x = int(world.grid/2)
+   plr[p].y = int(world.grid/2)
 plr[0].mapColor = (255,0,0)
 if( world.players > 1):
    plr[1].controls = {'up':K_w,'down':K_s,'left':K_a,'right':K_d}
@@ -173,6 +239,7 @@ if( world.players > 3):
 
 #game variables
 random.seed()
+l.generate()
 playing = 1
 update = 1 #flag for updating screen
 
@@ -188,24 +255,34 @@ while(playing):
          if key[K_SPACE]:
             l.clear()
             l.generate()
+            update = update+1
          for p in range(len(plr)):
-            update = update+plr[p].readInput(key)
+            update = update+(plr[p].readInput(key) == 0)
 
    if update:
+      if (debug): print("requests for frame update: %i" % (update))
+      
+      #move mobs
+      for m in range(len(l.mob)):
+         l.mob[m].moveTowards(plr[0].x,plr[0].y)
+
+      #draw player views
       for p in range(len(plr)):
+         if (debug): print("player %i view updated" % (p))
          plr[p].updateView()
          plr[p].drawView()
          screen.blit(plr[p].perspective,world.playerWindowPosition[p])
 
-      #draw the players on the map
+      #draw minimap
+      screen.blit(world.miniMap,world.miniMapPosition)
+
+      #draw the players on the minimap
       for p in range(len(plr)):
          pygame.draw.rect(world.miniMap, plr[p].mapColor, pygame.Rect( world.squareSize*world.mapScale*plr[p].x, world.squareSize*plr[p].y*world.mapScale, world.squareSize*world.mapScale, world.squareSize*world.mapScale))
 
-      screen.blit(world.miniMap,world.miniMapPosition)
-
       pygame.display.flip()
       update = 0 #clar flag for screen refresh
-   print("fps: %.f" % (1/(time.clock()-framecounter)))
+      if (debug): print("frame time: %.5f" % ((time.clock()-framecounter)))
 
 #cleanup
 pygame.display.quit()
